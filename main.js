c6b4f804f56e5372d69eb313f30ba53a25463285
henryhyde <serestrepov@gmail.com>
2023-01-15 16:20:23 -0500

    Navbar with interaction updated

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopEmail);
+burgerMenu.addEventListener('click', toggleBurgerMenu);
 
 function toggleDesktopEmail(){
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive')
+}
+
+function toggleBurgerMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
