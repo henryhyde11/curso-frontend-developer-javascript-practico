4c370f65ee88253637ca32937118b92295004ca1
henryhyde <serestrepov@gmail.com>
2023-01-22 23:09:07 -0500

    Aside improved

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleDesktopEmail);
 burgerMenu.addEventListener('click', toggleBurgerMenu);
+carMenu.addEventListener('click', toggleCarMenu);
 
 function toggleDesktopEmail(){
     desktopMenu.classList.toggle('inactive')
@@ -12,4 +15,8 @@ function toggleDesktopEmail(){
 
 function toggleBurgerMenu(){
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarMenu(){
+    aside.classList.toggle('inactive')
 }
